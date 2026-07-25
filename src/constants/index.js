4fdc7633550e620d8ea880909a92228b1927cde2
ErickJamesRubin <rubin.erick123@gmail.com>
2026-07-25 23:13:29 +0800

    Added contants folder and index js into it

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const ROLES = {
+  STUDENT: 'Student',
+  INSTRUCTOR: 'Instructor',
+  ADMIN: 'System Admin',
+}
+
+export const ACCOUNT_STATUS = {
+  ACTIVE: 'active',
+  INACTIVE: 'inactive',
+  PENDING: 'pending',
+}
