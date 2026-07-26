389699e2ace3533b8e1922c241d828954f433438
ErickJamesRubin <rubin.erick123@gmail.com>
2026-07-26 11:45:13 +0800

    Changing the right email for instructos

--- src/data/instructors.js
+++ src/data/instructors.js
@@ -3,7 +3,7 @@ export const instructors = [
   {
     id: 'INS-0001',
     name: 'Juan Dela Cruz',
-    email: 'juan.delacruz@nbsc.edu.ph',
+    email: 'delacruz.j@nbsc.edu.ph',
     department: 'Institute for Computer Studies (ICS)',
     status: 'active',
     linkedSections: ['IT311', 'IT301'],
@@ -11,7 +11,7 @@ export const instructors = [
   {
     id: 'INS-0002',
     name: 'Ana Marie Villanueva',
-    email: 'ana.villanueva@nbsc.edu.ph',
+    email: 'villanueva.a@nbsc.edu.ph',
     department: 'Institute of Business Management (IBM)',
     status: 'active',
     linkedSections: ['IBM015'],
@@ -19,7 +19,7 @@ export const instructors = [
   {
     id: 'INS-0003',
     name: 'Mark Anthony Lopez',
-    email: 'mark.lopez@nbsc.edu.ph',
+    email: 'lopez.m@nbsc.edu.ph',
     department: 'Institute of Teacher Education (ITE)',
     status: 'pending',
     linkedSections: [],
@@ -27,7 +27,7 @@ export const instructors = [
   {
     id: 'INS-0004',
     name: 'Kristine Joy Fernandez',
-    email: 'kristine.fernandez@nbsc.edu.ph',
+    email: 'fernandez.k@nbsc.edu.ph',
     department: 'Institute for Computer Studies (ICS)',
     status: 'active',
     linkedSections: ['IT042', 'IT088'],
