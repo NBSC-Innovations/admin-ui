c683cd5137b510395057f138913ed5a080e7a7a7
ErickJamesRubin <rubin.erick123@gmail.com>
2026-07-26 10:43:12 +0800

    Added class sections data

--- src/data/classSections.js
+++ src/data/classSections.js
@@ -1,1 +1,5 @@
-export const classSections = []
+// Shape: { id, sectionCode, subjectCode, section, instructor, schedule, room, memberCount }
+export const classSections = [
+  { id: 'CS-001', sectionCode: 'IT042', subjectCode: 'IT111', section: 'BSIT 1A', instructor: 'Juan Dela Cruz', schedule: 'MWF 7:30-9:00 AM', room: 'ICT-2', memberCount: 32 },
+  { id: 'CS-002', sectionCode: 'IBM015', subjectCode: 'BM101', section: 'BSBM 1A', instructor: 'Maria Santos', schedule: 'TTH 9:00-10:30 AM', room: 'Room 204', memberCount: 28 },
+]
