09b29ac74ff8c3c2d4d350ec3c04fbc7fea2756b
ErickJamesRubin <rubin.erick123@gmail.com>
2026-07-29 22:40:59 +0800

    Added more departments

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,4 +14,9 @@ export const DEPARTMENTS = {
   IBM: 'Institute of Business Management (IBM)',
   ICS: 'Institute for Computer Studies (ICS)',
   ITE: 'Institute of Teacher Education (ITE)',
+  DGEC: 'Department of General Education and Culture (DGEC)',
+  CCS: 'College of Criminal Justice and Sciences (CCS)',
+  RSS: 'Research and Statistics Services (RSS)',
+  NSTP: 'National Service Training Program (NSTP)',
+  PATHFIT: 'Physical Fitness and Health (PATHFIT)',
 }
